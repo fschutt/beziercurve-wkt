eb7ec2c0f2f34e296c1e906280a54525bf29f6fb
Felix Schütt <felix.schuett@maps4print.com>
2020-04-12 13:48:19 +0200

    Added (non-translated) JS algorithm for line-curve intersection

--- src/intersection.rs
+++ src/intersection.rs
@@ -2,8 +2,9 @@
 
 use crate::Point;
 
-pub type CubicCurve = (Point, Point, Point, Point);
-pub type Line = (Point, Point);
+pub type Line           = (Point, Point);
+pub type QuadraticCurve = (Point, Point, Point);
+pub type CubicCurve     = (Point, Point, Point, Point);
 
 #[derive(Debug, Copy, Clone, PartialEq, PartialOrd)]
 pub struct CubicCubicIntersection {
@@ -75,6 +76,119 @@ pub fn cubic_line_intersect(
     (b1, b2): Line,
 ) -> IntersectionResult {
     return IntersectionResult::NoIntersection; // TODO!
+
+    /*
+    // based on http://mysite.verizon.net/res148h4j/javascript/script_exact_cubic.html#the%20source%20code
+    function cubicRoots(P)
+    {
+        var a=P[0];
+        var b=P[1];
+        var c=P[2];
+        var d=P[3];
+
+        var A=b/a;
+        var B=c/a;
+        var C=d/a;
+
+        var Q, R, D, S, T, Im;
+
+        var Q = (3*B - Math.pow(A, 2))/9;
+        var R = (9*A*B - 27*C - 2*Math.pow(A, 3))/54;
+        var D = Math.pow(Q, 3) + Math.pow(R, 2);    // polynomial discriminant
+
+        var t=Array();
+
+        if (D >= 0)                                 // complex or duplicate roots
+        {
+            var S = sgn(R + Math.sqrt(D))*Math.pow(Math.abs(R + Math.sqrt(D)),(1/3));
+            var T = sgn(R - Math.sqrt(D))*Math.pow(Math.abs(R - Math.sqrt(D)),(1/3));
+
+            t[0] = -A/3 + (S + T);                    // real root
+            t[1] = -A/3 - (S + T)/2;                  // real part of complex root
+            t[2] = -A/3 - (S + T)/2;                  // real part of complex root
+            Im = Math.abs(Math.sqrt(3)*(S - T)/2);    // complex part of root pair
+
+            /*discard complex roots*/
+            if (Im!=0)
+            {
+                t[1]=-1;
+                t[2]=-1;
+            }
+
+        }
+        else                                          // distinct real roots
+        {
+            var th = Math.acos(R/Math.sqrt(-Math.pow(Q, 3)));
+
+            t[0] = 2*Math.sqrt(-Q)*Math.cos(th/3) - A/3;
+            t[1] = 2*Math.sqrt(-Q)*Math.cos((th + 2*Math.PI)/3) - A/3;
+            t[2] = 2*Math.sqrt(-Q)*Math.cos((th + 4*Math.PI)/3) - A/3;
+            Im = 0.0;
+        }
+
+        /*discard out of spec roots*/
+        for (var i=0;i<3;i++)
+            if (t[i]<0 || t[i]>1.0) t[i]=-1;
+
+        /*sort but place -1 at the end*/
+        t=sortSpecial(t);
+
+        console.log(t[0]+" "+t[1]+" "+t[2]);
+        return t;
+    }
+    */
+
+    /*
+    //computes intersection between a cubic spline and a line segment
+    function computeIntersections(px,py,lx,ly)
+    {
+        var X=Array();
+
+        var A=ly[1]-ly[0];      //A=y2-y1
+        var B=lx[0]-lx[1];      //B=x1-x2
+        var C=lx[0]*(ly[0]-ly[1]) +
+              ly[0]*(lx[1]-lx[0]);  //C=x1*(y1-y2)+y1*(x2-x1)
+
+        var bx = bezierCoeffs(px[0],px[1],px[2],px[3]);
+        var by = bezierCoeffs(py[0],py[1],py[2],py[3]);
+
+        var P = Array();
+        P[0] = A*bx[0]+B*by[0];     /*t^3*/
+        P[1] = A*bx[1]+B*by[1];     /*t^2*/
+        P[2] = A*bx[2]+B*by[2];     /*t*/
+        P[3] = A*bx[3]+B*by[3] + C; /*1*/
+
+        var r=cubicRoots(P);
+
+        /*verify the roots are in bounds of the linear segment*/
+        for (var i=0;i<3;i++)
+        {
+            t=r[i];
+
+            X[0]=bx[0]*t*t*t+bx[1]*t*t+bx[2]*t+bx[3];
+            X[1]=by[0]*t*t*t+by[1]*t*t+by[2]*t+by[3];
+
+            /*above is intersection point assuming infinitely long line segment,
+              make sure we are also in bounds of the line*/
+            var s;
+            if ((lx[1]-lx[0])!=0)           /*if not vertical line*/
+                s=(X[0]-lx[0])/(lx[1]-lx[0]);
+            else
+                s=(X[1]-ly[0])/(ly[1]-ly[0]);
+
+            /*in bounds?*/
+            if (t<0 || t>1.0 || s<0 || s>1.0)
+            {
+                X[0]=-100;  /*move off screen*/
+                X[1]=-100;
+            }
+
+            /*move intersection point*/
+            I[i].setAttributeNS(null,"cx",X[0]);
+            I[i].setAttributeNS(null,"cy",X[1]);
+        }
+    }
+    */
 }
 
 //  Determines the intersection point of the line defined by points A and B with the
@@ -83,14 +197,17 @@ pub fn cubic_line_intersect(
 //  Returns YES if the intersection point was found, and stores that point in X,Y.
 //  Returns NO if there is no determinable intersection point, in which case X,Y will
 //  be unmodified.
+#[inline]
 pub fn line_line_intersect(
 ) -> IntersectionResult {
-    return IntersectionResult::NoIntersection;
-/*
-      double  distAB, theCos, theSin, newX, ABpos ;
 
-      //  Fail if either line is undefined.
+    return IntersectionResult::NoIntersection; // TODO
+/*
+       //  Fail if either line is undefined.
       if (Ax==Bx && Ay==By || Cx==Dx && Cy==Dy) return IntersectionResult::NoIntersection;
+
+
+      double  distAB, theCos, theSin, newX, ABpos ;
 
       //  (1) Translate the system so that point A is on the origin.
       Bx-=Ax; By-=Ay;
@@ -122,10 +239,19 @@ pub fn line_line_intersect(
 */
 }
 
-// // Convert a quadratic bezier into a cubic bezier
-// fn quad_into_cubic_bezier(c: (Point, Point, Point)) -> CubicCurve {
-//
-// }
+// Convert a quadratic bezier into a cubic bezier
+#[inline]
+fn quad_into_cubic_bezier(c: QuadraticCurve) -> CubicCurve {
+    const TWO_THIRDS f32 = 2.0 / 3.0;
+
+    let c1_x = c.0.x + TWO_THIRDS * (c.1.x - c.0.x);
+    let c1_y = c.0.y + TWO_THIRDS * (c.1.y - c.0.y);
+
+    let c2_x = c.2.x + TWO_THIRDS * (c.1.x - c.2.x);
+    let c2_y = c.2.y + TWO_THIRDS * (c.1.y - c.2.y);
+
+    (c.0, Point::new(c1_x, c1_y), Point::new(c2_x, c2_y), c.3)
+}
 
 /// Bezier curve intersection algorithm and utilities
 /// Directly extracted from PaperJS's implementation bezier curve fat-line clipping
